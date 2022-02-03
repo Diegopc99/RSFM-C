00c0eec9ff5f7412e028ffacfb46edc7dff9bb66
Diego <diegopc99@gmail.com>
2022-02-03 18:57:12 +0100

    .

--- Python/script_init_total.sh
+++ Python/script_init_total.sh
@@ -8,6 +8,6 @@ python3 /home/pi/Python/subscriber.py &
 
 python3 /home/pi/Python/publisher.py &
 
-ffmpeg -f v4l2 -i /dev/video0 -c:v libx264 -preset ultrafast -tune zerolatency -b:v 600k -f rtsp -rtsp_transport tcp rtsp://diego1432.duckdns.org:7000/video &
+ffmpeg -f v4l2 -i /dev/video0 -c:v libx264 -preset ultrafast -tune zerolatency -b:v 600k -f rtsp -rtsp_transport tcp rtsp://domain/video &
 
 
